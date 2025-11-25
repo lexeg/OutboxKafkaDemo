fbd893b081d1562d19630fe9abad9ab979dbe04e
petrenko-a <petrenko-a@speechpro.com>
2025-11-25 19:16:59 +0500

    Сделал отправку в несколько потоков

--- OutboxKafkaConsole/Program.cs
+++ OutboxKafkaConsole/Program.cs
@@ -37,15 +37,23 @@ class Program
 
     private static async Task SendMessages(string createOrderRequest, List<string> messages)
     {
+        var chunks = Enumerable.Range(1, messages.Count).Chunk(messages.Count / Environment.ProcessorCount);
+        var tasks = chunks.Select(chunk => Send(createOrderRequest, messages, chunk)).ToList();
+        await Task.WhenAll(tasks);
+    }
+
+    private static async Task Send(string createOrderRequest, List<string> messages, int[] chunk)
+    {
         var client = new HttpClient();
-        foreach (var message in messages)
+        foreach (var ch in chunk)
         {
+            var message = messages[ch];
             var request = new HttpRequestMessage(HttpMethod.Post, createOrderRequest);
             var content = new StringContent(message, null, "application/json");
             request.Content = content;
             var response = await client.SendAsync(request);
             response.EnsureSuccessStatusCode();
-            Console.WriteLine(await response.Content.ReadAsStringAsync());
+            Console.WriteLine($"Order {ch} has been sent successfully.");
         }
     }
 }
